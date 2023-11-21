ec1617db6d3b378e354d2cd6a52b91c26cd04f11
Gabriel-Balbao <142052169+Gabriel-Balbao@users.noreply.github.com>
2023-11-21 15:18:10 -0300

    Adicionado diretório para o segundo trabalho

---
+++ Segundo Trabalho ED3/registros_indice.h
@@ -0,0 +1,37 @@
+/*
+Nome: Gabriel Balbão Bazon - NUSP: 13676408
+Nome: Giovanna de Freitas Velasco - NUSP: 13676346
+*/
+
+#include <stdio.h>
+
+#define TAM_REGISTRO_INDICES 205
+#define TAM_CAMPO_INDICES 55
+
+typedef struct{
+    char status;
+    int noRaiz;
+    int RRNproxNo;
+} cabecalhoIndice;
+
+
+typedef struct{
+    int nroChaveNo;
+    int alturaNo;
+    int RRNdoNo;
+
+    int P[4]; // ponteiro para uma subárvore
+    char C[3][55]; // chave de busca - tamanho 55
+    int PR[3]; // campo de referencia para o registro de dados
+} paginaIndice;
+
+
+void EscreveCabecalhoIndice(FILE *bin, cabecalhoIndice cabInd);
+
+void EscrevePagina(FILE *bin, paginaIndice pagInd);
+
+void LeCabecalhoIndice(FILE *bin, cabecalhoIndice *cabInd);
+
+void LePaginaIndice(FILE *bin, paginaIndice *pagInd);
+
+void ImprimePaginaIndice(paginaIndice *pagInd);
